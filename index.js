ef144b4f5ecfb0644378cc03a7fb4c54188313af
dhanraj0202 <dhanrajbeniwal34@gmail.com>
2025-03-20 14:29:39 +0530

    Max hours added

--- index.js
+++ index.js
@@ -56,9 +56,22 @@ function getWorkingHours(empCheck){
 const NUM_OF_WORKING_DAYS = 20;
 let empHrs = 0;
 
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
     var empCheck = Math.floor(Math.random()*10)%3;
     empHrs += getWorkingHours(empCheck);
 }
+// let empWage = empHrs*WAGE_PER_HOUR;
+// console.log("Total Hrs: " + empHrs + " Emp Wage: " + empWage);
+
+// UC5 (max hours and max days implemented)
+const MAX_HRS_IN_MONTH = 160;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    var empCheck = Math.floor(Math.random()*10)%3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+
 let empWage = empHrs*WAGE_PER_HOUR;
-console.log("Total Hrs: " + empHrs + " Emp Wage: " + empWage);
+console.log("UC5 - Total Days: " + totalWorkingDays +" Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
