6665fe6aa2410f265098755aafa06371534d2f7d
dhanraj0202 <dhanrajbeniwal34@gmail.com>
2025-03-20 23:43:17 +0530

    Daily wage and hour map using arrow functions

--- index.js
+++ index.js
@@ -84,6 +84,7 @@ function calcDailyWage(empHrs) {
 }
 let empDailyWageArr = new Array();
 let empDailyWageMap = new Map();
+let empDailyHrsMap = new Map();
 while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random()*10)%3;
@@ -91,6 +92,7 @@ while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
     totalEmpHrs += empHrs;
     empDailyWageArr.push(calcDailyWage(empHrs));
     empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
+    empDailyHrsMap.set(totalWorkingDays, empHrs);
 }
 let empWage = calcDailyWage(totalEmpHrs);
 // console.log("UC6 - Total Days: " + totalWorkingDays +" Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
@@ -155,3 +157,29 @@ function totalDaysWorked(numOfDays,dailyWage) {
     return numOfDays;
 }
 console.log("UC7G - Number of Days Emp Worked: " + empDailyWageArr.reduce(totalDaysWorked,0));
+
+// UC9 Arrow Functions
+const findTotal = (totalVal,dailyVal) =>{
+    return totalVal+dailyVal;
+}
+let count = 0;
+let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal,0);
+let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage>0).reduce(findTotal,0);
+console.log("UC9A - Emp Wage with Arrow: " + "Total Hours: " + totalHours + " Total Wages: " + totalSalary);
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+empDailyHrsMap.forEach((value,key,map) => {
+    if(value==8){
+        fullWorkingDays.push(key);
+    }
+    else if (value==4) {
+        partWorkingDays.push(key);
+    }
+    else{
+        nonWorkingDays.push(key);
+    }
+});
+console.log(("Full Working Days: " + fullWorkingDays));
+console.log(("Part Working Days: " + partWorkingDays));
+console.log(("Non Working Days: " + nonWorkingDays));
