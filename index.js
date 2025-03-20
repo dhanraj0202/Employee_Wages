1d002abdd29828ce8c5d04dad818b26bffa9432a
dhanraj0202 <dhanrajbeniwal34@gmail.com>
2025-03-20 15:05:32 +0530

    Stored daily wage in an array

--- index.js
+++ index.js
@@ -69,11 +69,26 @@ for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
 const MAX_HRS_IN_MONTH = 160;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+// while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
+//     totalWorkingDays++;
+//     let empCheck = Math.floor(Math.random()*10)%3;
+//     totalEmpHrs += getWorkingHours(empCheck);
+// }
+
+// let empWage = empHrs*WAGE_PER_HOUR;
+// console.log("UC5 - Total Days: " + totalWorkingDays +" Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
+
+// UC6 (Save daily wage in an array)
+function calcDailyWage(empHrs) {
+    return empHrs*WAGE_PER_HOUR;
+}
+let empDailyWageArr = new Array();
 while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random()*10)%3;
-    totalEmpHrs += getWorkingHours(empCheck);
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
 }
-
-let empWage = empHrs*WAGE_PER_HOUR;
-console.log("UC5 - Total Days: " + totalWorkingDays +" Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
+let empWage = calcDailyWage(totalEmpHrs);
+console.log("UC6 - Total Days: " + totalWorkingDays +" Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
